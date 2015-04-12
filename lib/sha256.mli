271df6306442d0d54e66835bc6aa19b36fbd9483
Eyyub <eyyub.pangearaion@gmail.com>
2015-04-12 05:03:10 +0200

    fix typo

--- lib/sha256.mli
+++ lib/sha256.mli
@@ -1,7 +1,7 @@
 val encrypt : string -> string
 
 module Utils : sig
-  (** Take a SHA-1 digest and outputs an hexstring.                                               
+  (** Take a SHA-256 digest and outputs an hexstring.                                               
       case : Format printing. **)
   val sha256_to_hexstring :
     ?case:[< `Lower | `Upper > `Lower] -> string -> string
