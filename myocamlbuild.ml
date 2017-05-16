16de9fe8ad37011a612ada77c1480ac8e03772c9
Romain Calascibetta <romain.calascibetta@gmail.com>
2017-05-15 23:33:59 -0700

    Change how to build digestif project

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,26 +1,27 @@
 open Ocamlbuild_plugin
 
 let headers =
-  [ "lib/native/digestif.h"
-  ; "lib/native/bitfn.h"
-  ; "lib/native/blake2b.h"
-  ; "lib/native/md5.h"
-  ; "lib/native/sha1.h"
-  ; "lib/native/sha256.h"
-  ; "lib/native/sha512.h" ]
+  [ "src-c/native/digestif.h"
+  ; "src-c/native/bitfn.h"
+  ; "src-c/native/blake2b.h"
+  ; "src-c/native/md5.h"
+  ; "src-c/native/sha1.h"
+  ; "src-c/native/sha256.h"
+  ; "src-c/native/sha512.h" ]
 
 let static = true
 
 let () =
   dispatch @@ function
   | After_rules ->
-    if static then flag ["link"; "ocaml"; "byte"] (A "-custom");
-
-    flag ["link"; "ocaml"; "library"; "byte"; "use_librakia_stubs"]
-      (S [A "-dllib"; A "-lrakia_stubs"; A "-cclib"; A "-lrakia_stubs"]);
-    flag ["link"; "ocaml"; "library"; "native"; "use_librakia_stubs"]
-      (S [A "-cclib"; A "-lrakia_stubs"]);
-
+    dep ["record_digestif_rakia_stubs"] ["src-c/librakia_stubs.a"];
     dep ["compile"; "c"] headers;
-    dep ["link"; "ocaml"; "use_librakia_stubs"] ["lib/librakia_stubs.a"]
+
+    flag_and_dep
+      ["link"; "ocaml"; "link_digestif_rakia_stubs"]
+      (P "src-c/librakia_stubs.a");
+    flag ["library"; "ocaml"; "byte"; "record_digestif_rakia_stubs"]
+      (S [ A "-dllib"; A "-lrakia_stubs"]);
+    flag ["library"; "ocaml"; "record_digestif_rakia_stubs"]
+      (S [ A "-cclib"; A "-lrakia_stubs"]);
   | _ -> ()
--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -7,17 +7,16 @@
 open Topkg
 
 let opam = Pkg.opam_file ~lint_deps_excluding:(Some [ "ocamlbuild"; "topkg"; "ocaml"; "ocamlfind" ]) "opam"
-let alcotest = Conf.with_pkg "alcotest"
 
 let () =
   Pkg.describe ~opams:[opam] "digestif" @@ fun c ->
 
-  let alcotest = Conf.value c alcotest in
-
   Ok [ Pkg.lib "pkg/META"
      ; Pkg.doc "README.md"
      ; Pkg.doc "CHANGES.md"
-     ; Pkg.clib "lib/librakia_stubs.clib"
-     ; Pkg.lib ~exts:Exts.module_library "lib/digestif"
-     ; Pkg.lib ~exts:Exts.module_library "lib/rakia"
-     ; Pkg.test ~cond:alcotest "test/test" ]
+
+     (* c implementation *)
+     ; Pkg.clib "src-c/librakia_stubs.clib" ~lib_dst_dir:"c"
+     ; Pkg.lib ~exts:Exts.module_library "src-c/digestif" ~dst:"c/digestif"
+
+     ; Pkg.lib ~exts:Exts.module_library "src-ocaml/digestif" ~dst:"ocaml/digestif" ]
