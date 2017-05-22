2ec310154228621cc916468aadc658e630f03da5
Romain Calascibetta <romain.calascibetta@gmail.com>
2017-05-22 05:15:35 -0700

    Add SHA-384 implementation in OCaml

--- src-ocaml/baijiu_sha384.ml
+++ src-ocaml/baijiu_sha384.ml
@@ -12,27 +12,27 @@ end
 module Make (B : Baijiu_buffer.S)
   : S with type buffer = B.buffer and type t = B.buffer
   = struct
-  module SHA256 = Baijiu_sha256.Make (B)
+  module SHA512 = Baijiu_sha512.Make (B)
 
-  include SHA256
+  include SHA512
 
   let init () =
     let b = B.create 128 in
 
     B.fill b 0 128 '\x00';
 
-    { size = 0L
+    { size = [| 0L; 0L |]
     ; b
-    ; h = [| 0xc1059ed8l
-           ; 0x367cd507l
-           ; 0x3070dd17l
-           ; 0xf70e5939l
-           ; 0xffc00b31l
-           ; 0x68581511l
-           ; 0x64f98fa7l
-           ; 0xbefa4fa4l |] }
+    ; h = [| 0xcbbb9d5dc1059ed8L
+           ; 0x629a292a367cd507L
+           ; 0x9159015a3070dd17L
+           ; 0x152fecd8f70e5939L
+           ; 0x67332667ffc00b31L
+           ; 0x8eb44a8768581511L
+           ; 0xdb0c2e0d64f98fa7L
+           ; 0x47b5481dbefa4fa4L |] }
 
   let get ctx =
     let res = get ctx in
-    B.sub res 0 28
+    B.sub res 0 48
 end
