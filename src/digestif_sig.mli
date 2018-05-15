80fbd262f119916081a7aa38a85319dfee89ad5b
Calascibetta Romain <romain.calascibetta@gmail.com>
2018-05-15 12:01:02 +0200

    Add documentation

--- src/digestif_sig.mli
+++ src/digestif_sig.mli
@@ -4,55 +4,150 @@ module Bytes = Digestif_bytes
 module type S = sig
 
   val digest_size : int
+  (** Size of the resulted hash in byte. *)
 
   module Bigstring:  sig
     type buffer = Bigstring.t
+    (** Type of input to digest. *)
+
     type ctx
+    (** Impure context (or abstract state) to feed it and then, return hash. *)
+
     type t = Bigstring.t
+    (** Type of the resulted hash. *)
 
-    val init           : unit -> ctx
-    val feed           : ctx -> buffer -> unit
-    val feed_bytes     : ctx -> Bytes.t -> unit
-    val feed_bigstring : ctx -> Bigstring.t -> unit
-    val get            : ctx -> t
+    val init: unit -> ctx
+    (** [init ()] initializes a new context to be feeded. *)
 
-    val digest         : buffer -> t
-    val digestv        : buffer list -> t
-    val hmac           : key:buffer -> buffer -> t
-    val hmacv          : key:buffer -> buffer list -> t
+    val feed: ctx -> buffer -> unit
+    (** [feed ctx input] feeds [ctx] with input. *)
 
-    val compare        : t -> t -> int
-    val eq             : t -> t -> bool
-    val neq            : t -> t -> bool
+    val feed_bytes: ctx -> Bytes.t -> unit
+    (** [feed_bytes ctx input] is same as {!feed} but it specialized with {Bytes.t}. *)
 
-    val pp             : Format.formatter -> t -> unit
-    val of_hex         : buffer -> t
-    val to_hex         : t -> buffer
+    val feed_bigstring: ctx -> Bigstring.t -> unit
+    (** [feed_bigstring ctx input] is an alias of {!feed}. *)
+
+    val get: ctx -> t
+    (** [get ctx] returns resulted hash of several feeding calls on {!ctx}. To
+       be care, this function has a side-effect on [ctx], that means [with ctx
+       == ctx, get ctx <> get ctx]. *)
+
+    val digest: buffer -> t
+    (** [digest input] returns directly resulted hash from [input]. *)
+
+    val digestv: buffer list -> t
+    (** [digestv inputs] returns directly resulted hash from multiple [inputs].
+       It's equivalent to get a new context, feed it successively with [inputs]
+       and return resulted hash. *)
+
+    val hmac: key:buffer -> buffer -> t
+    (** [hmac ~key input] returns a keyed-hash with [key] from [input]. We
+       follow {{:https://tools.ietf.org/html/rfc2104}RFC2104} (MD5) and
+       {{:https://tools.ietf.org/html/rfc4231}RFC4231} (SHA) and apply XOR
+       operation on {i ipad} and {i opad} values with [key]. *)
+
+    val hmacv: key:buffer -> buffer list -> t
+    (** [hmacv ~key inputs] has the same behavior of {digestv} but we return a
+       keyed-hash instead a hash (as {!hmac}). *)
+
+    val compare: t -> t -> int
+    (** [compare a b] compares [a] and [b] and return [0] if they are equal or
+       the difference of the first non-equal byte founded. *)
+
+    val eq: t -> t -> bool
+    (** [eq a b = compare a b = 0] *)
+
+    val neq: t -> t -> bool
+    (** [neq a b = not (eq a b)] *)
+
+    val pp: Format.formatter -> t -> unit
+    (** Pretty-printer of {!t}. Don't use it to produce an hexadecimal
+       representation of {!} (see {!to_hex}), this function is only usable to
+       print hash. *)
+
+    val of_hex: buffer -> t
+    (** [of_hex s] returns hash from hexadecimal representation [s]. If
+       [String.length s < digest_size], we fill it by [\x00]. [s] can have
+       whitespace characters, this is does not matter.
+
+      @raise Invalid_argument if [s] has non-authorized characters or number of
+       recognized characters is not a multiple of 2. *)
+
+    val to_hex: t -> buffer
+    (** [to_hex t] procudes a hexadecimal representation of {!t}. *)
   end
 
   module Bytes:  sig
     type buffer = Bytes.t
+    (** Type of input to digest. *)
+
     type ctx
+    (** Impure context (or abstract state) to feed it and then, return hash. *)
+
     type t = Bytes.t
+    (** Type of the resulted hash. *)
 
-    val init           : unit -> ctx
-    val feed           : ctx -> buffer -> unit
-    val feed_bytes     : ctx -> Bytes.t -> unit
-    val feed_bigstring : ctx -> Bigstring.t -> unit
-    val get            : ctx -> t
+    val init: unit -> ctx
+    (** [init ()] initializes a new context to be feeded. *)
 
-    val digest         : buffer -> t
-    val digestv        : buffer list -> t
-    val hmac           : key:buffer -> buffer -> t
-    val hmacv          : key:buffer -> buffer list -> t
+    val feed: ctx -> buffer -> unit
+    (** [feed ctx input] feeds [ctx] with input. *)
 
-    val compare        : t -> t -> int
-    val eq             : t -> t -> bool
-    val neq            : t -> t -> bool
+    val feed_bytes: ctx -> Bytes.t -> unit
+    (** [feed_bytes ctx input] is an alias of {!feed}. *)
 
-    val pp             : Format.formatter -> t -> unit
-    val of_hex         : buffer -> t
-    val to_hex         : t -> buffer
+    val feed_bigstring: ctx -> Bigstring.t -> unit
+    (** [feed_bigstring ctx input] is same as {!feed} but it specialized with {!Bigstring.t}. *)
+
+    val get: ctx -> t
+    (** [get ctx] returns resulted hash of several feeding calls on {!ctx}. To
+       be care, this function has a side-effect on [ctx], that means [with ctx
+       == ctx, get ctx <> get ctx]. *)
+
+    val digest: buffer -> t
+    (** [digest input] returns directly resulted hash from [input]. *)
+
+    val digestv: buffer list -> t
+    (** [digestv inputs] returns directly resulted hash from multiple [inputs].
+       It's equivalent to get a new context, feed it successively with [inputs]
+       and return resulted hash. *)
+
+    val hmac: key:buffer -> buffer -> t
+    (** [hmac ~key input] returns a keyed-hash with [key] from [input]. We
+       follow {{:https://tools.ietf.org/html/rfc2104}RFC2104} (MD5) and
+       {{:https://tools.ietf.org/html/rfc4231}RFC4231} (SHA) and apply XOR
+       operation on {i ipad} and {i opad} values with [key]. *)
+
+    val hmacv: key:buffer -> buffer list -> t
+    (** [hmacv ~key inputs] has the same behavior of {digestv} but we return a
+       keyed-hash instead a hash (as {!hmac}). *)
+
+    val compare: t -> t -> int
+    (** [compare a b] compares [a] and [b] and return [0] if they are equal or
+       the difference of the first non-equal byte founded. *)
+
+    val eq: t -> t -> bool
+    (** [eq a b = compare a b = 0] *)
+
+    val neq: t -> t -> bool
+    (** [neq a b = not (eq a b)] *)
+
+    val pp: Format.formatter -> t -> unit
+    (** Pretty-printer of {!t}. Don't use it to produce an hexadecimal
+       representation of {!} (see {!to_hex}), this function is only usable to
+       print hash. *)
+
+    val of_hex: buffer -> t
+    (** [of_hex s] returns hash from hexadecimal representation [s]. If
+       [String.length s < digest_size], we fill it by [\x00]. [s] can have
+       whitespace characters, this is does not matter.
+
+      @raise Invalid_argument if [s] has non-authorized characters or number of
+       recognized characters is not a multiple of 2. *)
+
+    val to_hex: t -> buffer
+    (** [to_hex t] procudes a hexadecimal representation of {!t}. *)
   end
 end
 
@@ -66,6 +161,7 @@ type hash =
   | SHA512
   | BLAKE2B of int
   | BLAKE2S of int
+  (** Hash algorithms *)
 
 module type C = sig
   val md5     : hash
