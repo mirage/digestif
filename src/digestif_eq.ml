4f4dc7a22ca877f62dd9ab2d8aefabfb4fcea23e
Calascibetta Romain <romain.calascibetta@gmail.com>
2018-09-04 17:36:34 +0200

    Use eqaf package to have a linear time equal function

--- src/digestif_eq.ml
+++ src/digestif_eq.ml
@@ -1,16 +1,7 @@
 module Make (D : sig val digest_size : int end) = struct
+  let _ = D.digest_size
 
-  (* XXX(dinosaure): these functions are implemented (instead to use common
-     functions) to avoid timing attacks. So if you want to update this code, you
-     need to take care about some assumptions. *)
-
-  let eq a b =
-    let ret = ref 0 in
-    for i = 0 to D.digest_size - 1
-    do ret := !ret lor ((Char.code (String.get a i)) lxor (Char.code (String.get b i))) done;
-    !ret <> 0
-
+  let eq a b = Eqaf.equal a b
   let neq a b = not (eq a b)
-
   let unsafe_compare a b = String.compare a b
 end
