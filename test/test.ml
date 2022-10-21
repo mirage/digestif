49754db2e8fcad94af0e7b6b84825aec83c4bc7c
Romain Calascibetta <romain.calascibetta@gmail.com>
2022-10-21 12:36:13 +0200

    Add a test about CVE-2022-37454

--- test/test.ml
+++ test/test.ml
@@ -78,7 +78,7 @@ let make_hmac :
     k Digestif.t ->
     unit Alcotest.test_case =
  fun ~name kind hash key input expect ->
-  (name, `Slow, fun () -> test_hmac kind hash key input expect)
+  (name, `Quick, fun () -> test_hmac kind hash key input expect)
 
 let make_digest :
     type a k.
@@ -89,7 +89,7 @@ let make_digest :
     k Digestif.t ->
     unit Alcotest.test_case =
  fun ~name kind hash input expect ->
-  (name, `Slow, fun () -> test_digest kind hash input expect)
+  (name, `Quick, fun () -> test_digest kind hash input expect)
 
 let combine a b c =
   let rec aux r a b c =
@@ -389,7 +389,7 @@ module BLAKE2 = struct
     | Bigstring -> check @@ Mac.maci_bigstring ~key (fun f -> f input)
 
   let make_keyed_blake m ~name kind hash key input expect =
-    (name, `Slow, fun () -> test_mac kind hash m key input expect)
+    (name, `Quick, fun () -> test_mac kind hash m key input expect)
 
   let tests m kind filename =
     let ic = open_in filename in
@@ -577,8 +577,6 @@ let keccak_vector_tests filename =
     try
       let comment = parse_field (input_line ic) in
       let message = parse_field (input_line ic) in
-      Fmt.epr ">>> %S.\n%!" comment ;
-      Fmt.epr ">>> %S.\n%!" (if message = empty then "" else of_hex message) ;
       let digest = (Digestif.of_hex hash <.> parse_field <.> input_line) ic in
       let _verify = input_line ic in
       let result =
@@ -591,6 +589,60 @@ let keccak_vector_tests filename =
     with End_of_file -> () in
   go () ;
   close_in ic
+
+external unsafe_set_uint8 : bigstring -> int -> int -> unit = "%caml_ba_set_1"
+
+external unsafe_set_uint32 : bigstring -> int -> int32 -> unit
+  = "%caml_bigstring_set32"
+
+let fill ba chr =
+  let len = Bigarray.Array1.dim ba in
+  let len0 = len land 3 in
+  let len1 = len asr 2 in
+  let v0 = Char.code chr in
+  let v1 = Int32.of_int v0 in
+
+  for i = 0 to len1 - 1 do
+    let i = i * 4 in
+    unsafe_set_uint32 ba i v1
+  done ;
+
+  for i = 0 to len0 - 1 do
+    let i = (len1 * 4) + i in
+    unsafe_set_uint8 ba i v0
+  done
+
+let sha3_cve_2022_37454_0 =
+  Alcotest.test_case "buffer overflow" `Slow @@ fun () ->
+  Gc.full_major () ;
+  let a = Bigarray.Array1.create Bigarray.char Bigarray.c_layout 1 in
+  let b = Bigarray.Array1.create Bigarray.char Bigarray.c_layout 4294967295 in
+  fill a '\x00' ;
+  fill b '\x00' ;
+  let ctx = Digestif.SHA3_224.empty in
+  let ctx = Digestif.SHA3_224.feed_bigstring ctx a in
+  let ctx = Digestif.SHA3_224.feed_bigstring ctx b in
+  let hash = Digestif.SHA3_224.get ctx in
+  Alcotest.(check (testable Digestif.SHA3_224.pp Digestif.SHA3_224.equal))
+    "result" hash
+    (Digestif.SHA3_224.of_hex
+       "c5bcc3bc73b5ef45e91d2d7c70b64f196fac08eee4e4acf6e6571ebe")
+
+let sha3_cve_2022_37454_1 =
+  Alcotest.test_case "infinite loop" `Slow @@ fun () ->
+  Gc.full_major () ;
+  let a = Bigarray.Array1.create Bigarray.char Bigarray.c_layout 1 in
+  let b = Bigarray.Array1.create Bigarray.char Bigarray.c_layout 4294967296 in
+  fill a '\x00' ;
+  fill b '\x00' ;
+  let ctx = Digestif.SHA3_224.empty in
+  let ctx = Digestif.SHA3_224.feed_bigstring ctx a in
+  let ctx = Digestif.SHA3_224.feed_bigstring ctx b in
+  let hash = Digestif.SHA3_224.get ctx in
+  Alcotest.(check (testable Digestif.SHA3_224.pp Digestif.SHA3_224.equal))
+    "result" hash
+    (Digestif.SHA3_224.of_hex
+       "bdd5167212d2dc69665f5a8875ab87f23d5ce7849132f56371a19096")
 
 let tests () =
   Alcotest.run "digestif"
@@ -697,6 +749,7 @@ let tests () =
           sha3_vector_tests "../sha3_512_fips_202.txt";
           keccak_vector_tests "../keccak_256.txt";
         ] );
+      ("sha3 (CVE-2022-37454)", [ sha3_cve_2022_37454_0; sha3_cve_2022_37454_1 ]);
     ]
 
 let () = tests ()
